afba2dd75238b32b25ca902c977612a57418cffe
ilopez-r <133966465+ilopez-r@users.noreply.github.com>
2024-01-08 17:03:32 +0100

    Update expand_str.c

--- Level 2/expand_str/expand_str.c
+++ Level 2/expand_str/expand_str.c
@@ -3,39 +3,34 @@
 int main (int argc, char **argv)
 {
 	int i;
-	int j;
-	if (argc != 2)
-	{
-		write (1, "\n", 1);
-		return (0);
-	}
-	i = 0;
-	j = 0;
-	while (argv[1][j] != '\0')
-		j++;
-	j--;
-	while (argv[1][j] == ' ' || argv[1][j] == '	')
-		j--;
-	if (i <= j)
-	{
-		while (argv[1][i] == ' ' || argv[1][i] == '	')
-			i++;
-		while (i <= j)
-		{
-			while (argv[1][i] == ' ' || argv[1][i] == '	')
-			{
-				if (argv[1][i + 1] == ' ' || argv[1][i + 1] == '	')
-					i++;
-				else
-				{
-					write(1, "   ", 3);
-					i++;
-				}
-			}
-			write(1, &argv[1][i], 1);
-			i++;
-		}
-	}
-	write (1, "\n", 1);
-	return(0);
+       	int j;
+  
+        i = 0;
+        j = 0;
+        if (argc == 2)
+        {
+            while (argv[1][j] != '\0')
+                j++;
+            j--;
+            while (argv[1][j] == ' ' || argv[1][j] == ' ')
+                j--;
+            while (argv[1][i] == ' ' || argv[1][i] == ' ')
+                i++;
+            while (i <= j)
+            {
+                while (argv[1][i] == ' ' || argv[1][i] == ' ')
+                {
+                    if(argv[1][i + 1] == ' ' || argv[1][i + 1] == ' ')
+                        i++;
+                    else
+                    {
+                        write (1, "   ", 3);
+                        i++;
+                    }
+                }
+                write (1, &argv[1][i], 1);
+                i++;
+            }
+        }
+        write (1, "\n", 1);
 }
