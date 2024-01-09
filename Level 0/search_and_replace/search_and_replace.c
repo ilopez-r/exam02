69ac408f0736ce55b320a1dcadec953274aea62b
ilopez-r <133966465+ilopez-r@users.noreply.github.com>
2024-01-09 13:13:07 +0100

    Update search_and_replace.c

--- Level 0/search_and_replace/search_and_replace.c
+++ Level 0/search_and_replace/search_and_replace.c
@@ -13,11 +13,10 @@ int main(int argc, char **argv)
    			j++;
 		if (j == 1)
 		{
-			j = 0;
 			while (argv[1][i] != '\0')
 			{
-				if (argv[1][i] == argv[2][j])
-					argv[1][i] = argv[3][j];
+				if (argv[1][i] == argv[2][0])
+					argv[1][i] = argv[3][0];
 				write(1, &argv[1][i], 1);
 				i++;
 			}
